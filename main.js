cd6e8d2fd1dad98109e2822166ad715ebfa7a221
Dave Harms <daveharmswebdev@gmail.com>
2016-12-04 14:50:10 -0600

    removed unnescessary console.logs

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const http = require('http')
 const getList = require('./list')
 const calc = require('./calc')
 
-//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 const options = {
   host: 'data.nashville.gov',
   path: '/resource/66wh-az4r.json'
@@ -13,17 +12,13 @@ const options = {
 const callback = function(response) {
   let str = '';
 
-  //another chunk of data has been recieved, so append it to `str`
   response.on('data', function (chunk) {
     str += chunk;
   });
 
-  //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
     const data = JSON.parse(str)
     const list = getList(data)
-    // console.log('list', list)
-    // console.log('list', calc(data, 'DRUG TASK FORCE'))
     list.forEach(department => console.log(department, calc(data, department)))
   });
 }
